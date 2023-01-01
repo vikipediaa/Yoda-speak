196834431d447b8a8b629a0322c13900d47386a9
Vikas Pandey <pandey222vikas@gmail.com>
2023-01-01 14:13:17 +0530

    Done css part

---
+++ app.js
@@ -0,0 +1,29 @@
+var  btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+ 
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+function getTranslationURL(input)
+{
+    return serverURL + "?" + "text="  + input
+};
+function errorHandler(error){
+    console.log("error occured",console.error);
+    alert("something went worng");
+}
+
+function clickHandler()
+{
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var tralatedText = json.contents.translated;
+        outputDiv.innerText = tralatedText;
+    }
+
+    )
+    .catch(errorHandler)
+};
+btnTranslate.addEventListener("click",clickHandler)
